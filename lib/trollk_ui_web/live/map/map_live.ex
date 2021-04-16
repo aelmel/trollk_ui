f433f93b9558eb2f69e310897550bc7d4d7eace4
Alexei Melnic <alexei.melnic@gmail.com>
2021-04-16 14:12:05 +0300

    add routes to map page

--- lib/trollk_ui_web/live/map/map_live.ex
+++ lib/trollk_ui_web/live/map/map_live.ex
@@ -1,6 +1,6 @@
 defmodule TrollkUiWeb.MapLive do
   use TrollkUiWeb, :live_view
-  @topic "route:8"
+  require Logger
 
   def mount(_params, _session, socket) do
     routes = Trollk.Routes.Api.get_routes()
@@ -10,6 +10,20 @@ defmodule TrollkUiWeb.MapLive do
 
   def render(assigns) do
     Phoenix.View.render(TrollkUiWeb.MapView, "map_live.html", assigns)
+  end
+
+  def handle_event("subscribe", %{"route-topic" => route_topic}, socket) do
+    Logger.info("Subscribe to route #{route_topic}")
+
+    case TrollkUi.Trollk.SocketClient.start_link(live_pid: self(), topic: route_topic) do
+      {:ok, _} ->
+        Logger.debug("Subscribe succesfully")
+
+      ex ->
+        Logger.warn("error received on subscribe #{inspect(ex)}")
+    end
+
+    {:noreply, socket}
   end
 
   def handle_info(event, socket) do
--- lib/trollk_ui_web/views/map_view.ex
+++ lib/trollk_ui_web/views/map_view.ex
@@ -1,3 +1,13 @@
 defmodule TrollkUiWeb.MapView do
   use TrollkUiWeb, :view
+
+  def get_route_button(%{"number" => number, "name" => name}) do
+    assigns = %{number: number, name: name, topic: "route:#{number}"}
+
+    ~L"""
+    <button class="css-framework-class" phx-click="subscribe" phx-value-route-topic="<%= @topic %>">
+        <%= @number %>
+    </button>
+    """
+  end
 end
