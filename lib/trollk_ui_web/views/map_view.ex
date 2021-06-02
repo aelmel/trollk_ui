6261d6a83363829b75c59f4dfe956b941b087040
Alexei Melnic <alexei.melnic@gmail.com>
2021-06-02 20:45:32 +0300

    set svg as symbol

--- lib/trollk_ui_web/views/map_view.ex
+++ lib/trollk_ui_web/views/map_view.ex
@@ -2,12 +2,11 @@ defmodule TrollkUiWeb.MapView do
   use TrollkUiWeb, :view
 
   def get_route_button(%{"number" => number, "name" => name, "color" => color} = params) do
-    IO.inspect(params)
     assigns = %{number: number, name: name, topic: "route:#{number}", color: color}
 
     ~L"""
     <button class="css-framework-class" phx-click="subscribe" phx-value-route-topic="<%= @topic %>" phx-value-route-color="<%= @color %>">
-        <%= @number %>
+        <%= String.pad_leading(@number, 2, " ") %>
     </button>
     """
   end
